bc372ed254591dd47fd2a02fa798291907b662fc
Adam Poulemanos <adam@plainlicense.org>
2025-06-19 07:42:32 -0400

    fix(infra): finally fixed some bugs in update_sponsors.py ci script)

--- .github/scripts/update_sponsors.py
+++ .github/scripts/update_sponsors.py
@@ -1,20 +1,53 @@
+"""
+Update the sponsors' donation markdown file.
+"""
 import os
 import re
 
-import requests
-import ez_yaml as yaml
+from pathlib import Path
+from typing import Any, TypedDict, TypeGuard
 
-DONATE_FILE = "docs/helping/donate.md"
+import ez_yaml as yaml
+import requests
+
+
+DONATE_FILE = Path.cwd() / "docs/helping/donate.md"
 GOAL = 5000
 ENDPOINT = "https://api.github.com/graphql"
-USERNAME = "seekinginfiniteloop"
+USERNAME = "bashandbone"
+
+
+class DonateFrontMatter(TypedDict):
+    """
+    TypedDict for the front matter of the donation markdown file.
+    """
+    title: str
+    description: str
+    template: str
+    funding_goal: int
+    funding_progress: int
+
+
+def frontmatter_guard(content: Any) -> TypeGuard[DonateFrontMatter]:
+    """
+    Type guard to check if the content is a valid DonateFrontMatter.
+
+    Args:
+        content (Any): The content to check.
+
+    Returns:
+        TypeGuard[DonateFrontMatter]: True if content is a valid DonateFrontMatter, False otherwise.
+    """
+    return isinstance(content, dict) and all(
+        key in content for key in ["title", "description", "template", "funding_goal", "funding_progress"]
+    )
 
 
 def fetch_total_amount(token: str) -> int:
     """
     Fetch the total amount of sponsorships received by a user from the GitHub API.
 
-    This function sends a GraphQL query to the GitHub API using the provided token to retrieve the total lifetime sponsorship values for a specified user. It returns the total amount in dollars by summing the amounts received in cents.
+    Sends a GraphQL query to the GitHub API to retrieve the lifetime sponsorship values for a specified user. It sums the amounts received in cents and returns the total amount in dollars.
 
     Args:
         token (str): The authorization token used to access the GitHub API.
@@ -22,18 +55,14 @@ def fetch_total_amount(token: str) -> int:
     Returns:
         int: The total amount of sponsorships received, in dollars.
     """
-
     url = "https://api.github.com/graphql"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json",
-    }
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
     query = (
         """
     {
     user(login: """
-        + f'"{USERNAME}"'
-        + """) {
+        f'"{USERNAME}"'
+        """) {
         lifetimeReceivedSponsorshipValues(first: 200) {
         edges {
             node {
@@ -55,25 +84,23 @@ def fetch_total_amount(token: str) -> int:
     """
     )
     try:
-        if response := requests.post(url, json={"query": query}, headers=headers):
+        if response := requests.post(url, json={"query": query}, headers=headers, timeout=15):
             cents = sum(
                 edge["node"]["amountInCents"]
-                for edge in response.json()["data"]["user"][
-                    "lifetimeReceivedSponsorshipValues"
-                ]["edges"]
+                for edge in response.json()["data"]["user"]["lifetimeReceivedSponsorshipValues"][
+                    "edges"
+                ]
             )
             return cents // 100
-        return 0
     except Exception as e:
         print(e)
         return 0
+    return 0
 
 
 def update_front_matter(content: str, amount: int) -> str:
     """
-    Update the front matter of a markdown document with a new funding progress amount.
-
-    This function extracts the existing front matter from the provided content, updates the funding progress with the specified amount, and reconstructs the document with the modified front matter.
+    Update the front matter of the donation markdown file with the new funding progress amount. Also sets the funding goal.
 
     Args:
         content (str): The original markdown content containing front matter.
@@ -82,7 +109,6 @@ def update_front_matter(content: str, amount: int) -> str:
     Returns:
         str: The updated markdown content with the modified front matter.
     """
-
     if front_matter_match := re.match(
         r"^---\s*\n(.*?)\n---\s*\n", content, re.DOTALL | re.MULTILINE
     ):
@@ -92,7 +118,11 @@ def update_front_matter(content: str, amount: int) -> str:
         front_matter = {}
         rest_of_document = content
 
-    front_matter["funding-progress"] = amount
+    if not frontmatter_guard(front_matter):
+        raise ValueError("Invalid front matter format.")
+
+    front_matter["funding_progress"] = amount
+    front_matter["funding_goal"] = GOAL
 
     # Convert the updated front matter back to YAML
     updated_front_matter = yaml.to_string(front_matter)
@@ -105,7 +135,7 @@ def main() -> None:
     """
     Main function to update the sponsors' donation markdown file.
 
-    This function retrieves the current total donation amount using a provided token, updates the markdown file with this information, and saves the changes.
+    Retrieves the total amount of sponsorships from the GitHub API and updates the front matter of the donation markdown file with this amount. Writes the updated content back to the file.
 
     Args:
         None
@@ -116,13 +146,15 @@ def main() -> None:
     if token := os.getenv("GH_TOKEN"):
         total_amount = fetch_total_amount(token)
 
-        with open("docs/donate.md", "r") as file:
-            content = file.read()
+        content = DONATE_FILE.read_text(encoding="utf-8")
 
         updated_content = update_front_matter(content, total_amount)
 
-        with open("docs/helping/donate.md", "w") as file:
-            file.write(updated_content)
+        DONATE_FILE.write_text(updated_content, encoding="utf-8")
+    else:
+        raise ValueError(
+            "Please set the GH_TOKEN environment variable with your GitHub token."
+        )
 
 
 if __name__ == "__main__":
